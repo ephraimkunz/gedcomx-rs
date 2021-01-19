bdf9815aa927fcac26e725f5ee8fca1acd9647d9
Ephraim Kunz <ephraimkunz@me.com>
2021-01-18 22:00:27 -0800

    More cleanup

--- gedcomx/src/components/fact.rs
+++ gedcomx/src/components/fact.rs
@@ -50,312 +50,117 @@ impl From<FactType> for FactTypeSerde {
 #[serde(from = "FactTypeSerde", into = "FactTypeSerde")]
 pub enum FactType {
     // Person fact types.
-    #[serde(rename = "http://gedcomx.org/Adoption")]
     Adoption,
-
-    #[serde(rename = "http://gedcomx.org/AdultChristening")]
     AdultChristening,
-
-    #[serde(rename = "http://gedcomx.org/Amnesty")]
     Amnesty,
-
-    #[serde(rename = "http://gedcomx.org/AncestralHall")]
     AncestralHall,
-
-    #[serde(rename = "http://gedcomx.org/AncestralPoem")]
     AncestralPoem,
-
-    #[serde(rename = "http://gedcomx.org/Apprenticeship")]
     Apprenticeship,
-
-    #[serde(rename = "http://gedcomx.org/Arrest")]
     Arrest,
-
-    #[serde(rename = "http://gedcomx.org/Award")]
     Award,
-
-    #[serde(rename = "http://gedcomx.org/Baptism")]
     Baptism,
-
-    #[serde(rename = "http://gedcomx.org/BarMitzvah")]
     BarMitzvah,
-
-    #[serde(rename = "http://gedcomx.org/BatMitzvah")]
     BatMitzvah,
-
-    #[serde(rename = "http://gedcomx.org/Birth")]
     Birth,
-
-    #[serde(rename = "http://gedcomx.org/BirthNotice")]
     BirthNotice,
-
-    #[serde(rename = "http://gedcomx.org/Blessing")]
     Blessing,
-
-    #[serde(rename = "http://gedcomx.org/Branch")]
     Branch,
-
-    #[serde(rename = "http://gedcomx.org/Burial")]
     Burial,
-
-    #[serde(rename = "http://gedcomx.org/Caste")]
     Caste,
-
-    #[serde(rename = "http://gedcomx.org/Census")]
     Census,
-
-    #[serde(rename = "http://gedcomx.org/Christening")]
     Christening,
-
-    #[serde(rename = "http://gedcomx.org/Circumcision")]
     Circumcision,
-
-    #[serde(rename = "http://gedcomx.org/Clan")]
     Clan,
-
-    #[serde(rename = "http://gedcomx.org/Confirmation")]
     Confirmation,
-
-    #[serde(rename = "http://gedcomx.org/Court")]
     Court,
-
-    #[serde(rename = "http://gedcomx.org/Cremation")]
     Cremation,
-
-    #[serde(rename = "http://gedcomx.org/Death")]
     Death,
-
-    #[serde(rename = "http://gedcomx.org/Education")]
     Education,
-
-    #[serde(rename = "http://gedcomx.org/EducationEnrollment")]
     EducationEnrollment,
-
-    #[serde(rename = "http://gedcomx.org/Emigration")]
     Emigration,
-
-    #[serde(rename = "http://gedcomx.org/Enslavement")]
     Enslavement,
-
-    #[serde(rename = "http://gedcomx.org/Ethnicity")]
     Ethnicity,
-
-    #[serde(rename = "http://gedcomx.org/Excommunication")]
     Excommunication,
-
-    #[serde(rename = "http://gedcomx.org/FirstCommunion")]
     FirstCommunion,
-
-    #[serde(rename = "http://gedcomx.org/Funeral")]
     Funeral,
-
-    #[serde(rename = "http://gedcomx.org/GenderChange")]
     GenderChange,
-
-    #[serde(rename = "http://gedcomx.org/GenerationNumber")]
     GenerationNumber,
-
-    #[serde(rename = "http://gedcomx.org/Graduation")]
     Graduation,
-
-    #[serde(rename = "http://gedcomx.org/Heimat")]
     Heimat,
-
-    #[serde(rename = "http://gedcomx.org/Immigration")]
     Immigration,
-
-    #[serde(rename = "http://gedcomx.org/Imprisonment")]
     Imprisonment,
-
-    #[serde(rename = "http://gedcomx.org/Inquest")]
     Inquest,
-
-    #[serde(rename = "http://gedcomx.org/LandTransaction")]
     LandTransaction,
-
-    #[serde(rename = "http://gedcomx.org/Language")]
     Language,
-
-    #[serde(rename = "http://gedcomx.org/Living")]
     Living,
-
-    #[serde(rename = "http://gedcomx.org/MaritalStatus")]
     MaritalStatus,
-
-    #[serde(rename = "http://gedcomx.org/Medical")]
     Medical,
-
-    #[serde(rename = "http://gedcomx.org/MilitaryAward")]
     MilitaryAward,
-
-    #[serde(rename = "http://gedcomx.org/MilitaryDischarge")]
     MilitaryDischarge,
-
-    #[serde(rename = "http://gedcomx.org/MilitaryDraftRegistration")]
     MilitaryDraftRegistration,
-
-    #[serde(rename = "http://gedcomx.org/MilitaryInduction")]
     MilitaryInduction,
-
-    #[serde(rename = "http://gedcomx.org/MilitaryService")]
     MilitaryService,
-
-    #[serde(rename = "http://gedcomx.org/Mission")]
     Mission,
-
-    #[serde(rename = "http://gedcomx.org/MoveFrom")]
     MoveFrom,
-
-    #[serde(rename = "http://gedcomx.org/MoveTo")]
     MoveTo,
-
-    #[serde(rename = "http://gedcomx.org/MultipleBirth")]
     MultipleBirth,
-
-    #[serde(rename = "http://gedcomx.org/NationalId")]
     NationalId,
-
-    #[serde(rename = "http://gedcomx.org/Nationality")]
     Nationality,
-
-    #[serde(rename = "http://gedcomx.org/Naturalization")]
     Naturalization,
-
-    #[serde(rename = "http://gedcomx.org/NumberOfChildren")]
     NumberOfChildren, // Also a couple fact type.
-
-    #[serde(rename = "http://gedcomx.org/NumberOfMarriages")]
     NumberOfMarriages,
-
-    #[serde(rename = "http://gedcomx.org/Obituary")]
     Obituary,
-
-    #[serde(rename = "http://gedcomx.org/OfficialPosition")]
     OfficialPosition,
-
-    #[serde(rename = "http://gedcomx.org/Occupation")]
     Occupation,
-
-    #[serde(rename = "http://gedcomx.org/Ordination")]
     Ordination,
-
-    #[serde(rename = "http://gedcomx.org/Pardon")]
     Pardon,
-
-    #[serde(rename = "http://gedcomx.org/PhysicalDescription")]
     PhysicalDescription,
-
-    #[serde(rename = "http://gedcomx.org/Probate")]
     Probate,
-
-    #[serde(rename = "http://gedcomx.org/Property")]
     Property,
-
-    #[serde(rename = "http://gedcomx.org/Race")]
     Race,
-
-    #[serde(rename = "http://gedcomx.org/Religion")]
     Religion,
-
-    #[serde(rename = "http://gedcomx.org/Residence")]
     Residence,
-
-    #[serde(rename = "http://gedcomx.org/Retirement")]
     Retirement,
-
-    #[serde(rename = "http://gedcomx.org/Stillbirth")]
     Stillbirth,
-
-    #[serde(rename = "http://gedcomx.org/TaxAssessment")]
     TaxAssessment,
-
-    #[serde(rename = "http://gedcomx.org/Tribe")]
     Tribe,
-
-    #[serde(rename = "http://gedcomx.org/Will")]
     Will,
-
-    #[serde(rename = "http://gedcomx.org/Visit")]
     Visit,
-
-    #[serde(rename = "http://gedcomx.org/Yahrzeit")]
     Yahrzeit,
 
     // Couple fact types.
-    #[serde(rename = "http://gedcomx.org/Annulment")]
     Annulment,
-
-    #[serde(rename = "http://gedcomx.org/CommonLawMarriage")]
     CommonLawMarriage,
-
-    #[serde(rename = "http://gedcomx.org/CivilUnion")]
     CivilUnion,
-
-    #[serde(rename = "http://gedcomx.org/Divorce")]
     Divorce,
-
-    #[serde(rename = "http://gedcomx.org/DivorceFiling")]
     DivorceFiling,
-
-    #[serde(rename = "http://gedcomx.org/DomesticPartnership")]
     DomesticPartnership,
-
-    #[serde(rename = "http://gedcomx.org/Engagement")]
     Engagement,
-
-    #[serde(rename = "http://gedcomx.org/Marriage")]
     Marriage,
-
-    #[serde(rename = "http://gedcomx.org/MarriageBanns")]
     MarriageBanns,
-
-    #[serde(rename = "http://gedcomx.org/MarriageContract")]
     MarriageContract,
-
-    #[serde(rename = "http://gedcomx.org/MarriageLicense")]
     MarriageLicense,
-
-    #[serde(rename = "http://gedcomx.org/MarriageNotice")]
     MarriageNotice,
-
-    #[serde(rename = "http://gedcomx.org/Separation")]
     Separation,
 
     // Parent-child fact types.
-    #[serde(rename = "http://gedcomx.org/AdoptiveParent")]
     AdoptiveParent,
-
-    #[serde(rename = "http://gedcomx.org/BiologicalParent")]
     BiologicalParent,
-
-    #[serde(rename = "http://gedcomx.org/ChildOrder")]
     ChildOrder,
-
-    #[serde(rename = "http://gedcomx.org/EnteringHeir")]
     EnteringHeir,
-
-    #[serde(rename = "http://gedcomx.org/ExitingHeir")]
     ExitingHeir,
-
-    #[serde(rename = "http://gedcomx.org/FosterParent")]
     FosterParent,
-
-    #[serde(rename = "http://gedcomx.org/GuardianParent")]
     GuardianParent,
-
-    #[serde(rename = "http://gedcomx.org/StepParent")]
     StepParent,
-
-    #[serde(rename = "http://gedcomx.org/SociologicalParent")]
     SociologicalParent,
-
-    #[serde(rename = "http://gedcomx.org/SurrogateParent")]
     SurrogateParent,
 
+    // Catch all
     Custom(Uri),
 }
 
 impl fmt::Display for FactType {
+    #[allow(clippy::too_many_lines)]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         match self {
             Self::Adoption => write!(f, "http://gedcomx.org/Adoption"),
@@ -466,6 +271,7 @@ impl fmt::Display for FactType {
 }
 
 impl From<FactTypeSerde> for FactType {
+    #[allow(clippy::too_many_lines)]
     fn from(f: FactTypeSerde) -> Self {
         // If you need to generate this mapping in the future, the easiest way is to copy and paste the tables in
         // https://github.com/FamilySearch/gedcomx/blob/master/specifications/fact-types-specification.md.
