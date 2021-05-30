edb202cdc6f53433706f13cd766daad4eb93a6aa
Ephraim Kunz <ephraimkunz@me.com>
2021-05-30 13:37:05 -0700

    Update cargo to pull from main yaserde git branch

--- gedcomx-cli/src/main.rs
+++ gedcomx-cli/src/main.rs
@@ -1,1 +1,5 @@
-fn main() {}
+fn main() {
+    let big_file = std::fs::read_to_string("data/parents_fs.xml").unwrap();
+    let gx = gedcomx::Gedcomx::from_xml_str(&big_file).unwrap();
+    println!("{}", gx.to_xml_string_pretty().unwrap())
+}
